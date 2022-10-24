85da69e04c0f26218d86ceac07f4e2e541e6d4d4
yaprincev <5489812@mail.ru>
2022-10-24 19:23:59 +0300

    Init MainVC

--- JokeApi/MainViewController.swift
+++ JokeApi/MainViewController.swift
@@ -8,22 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Views
+    
+    @IBOutlet private weak var textLabel: UILabel!
+    
+    @IBOutlet private weak var buttonLabel: UIButton!
+    
+    
+    // MARK: - UIViewController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+  
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Action
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        
     }
-    */
+    
+
 
 }
